1895c4d22ea5a7b6ef3267ccc35c5dbaeb23620f
xun liu <xunliu@xuns-MacBook-Pro.local>
2018-10-19 15:23:49 -0700

    Parser v1.0

---
+++ Parser/CMakeFiles/test_ast.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_ast.dir/test_ast.cpp.o"
+  "CMakeFiles/test_ast.dir/ASTNode.cpp.o"
+  "CMakeFiles/test_ast.dir/CodegenContext.cpp.o"
+  "test_ast.pdb"
+  "test_ast"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_ast.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
